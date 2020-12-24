c3d3f656ae7005e36273e5b0f79e8277a18cd2eb
Elijahscriptdev <elijahobominuru@gmail.com>
2020-12-24 04:05:16 +0100

    created hero component

---
+++ src/component/Hero/Hero.js
@@ -0,0 +1,19 @@
+import styles from './Hero.module.css'
+
+const Hero = () => {
+    return (
+        <div className={styles.hero_image}>
+            <h1 className={styles.hero_title}>Photography</h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
+import Hero from '../component/Hero/Hero'
 import Layout from '../component/Layout/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      <div>Photography</div>
+      <Hero />
     </Layout>
   )
 }
